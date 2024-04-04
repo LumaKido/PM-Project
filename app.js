cea06449887a2416b3a32b8148891f6620ecaf31
LumaKido <lumamari499@gmail.com>
2024-04-03 22:39:10 -0300

    verificacao

---
+++ app.js
@@ -0,0 +1,65 @@
+const express = require("express")
+const app = express()
+const handlebars = require("express-handlebars").engine
+const bodyParser = require("body-parser")
+const post = require("./models/post")
+
+app.engine("handlebars", handlebars({ defaultLayout: "main" }))
+app.set("view engine", "handlebars")
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.get("/", function (req, res) {
+  res.render("primeira_pagina")
+})
+   
+app.post("/cadastrar", function(req, res){
+  post.create({
+      nome: req.body.nome,
+      telefone: req.body.telefone,
+      origem: req.body.origem,
+      data_contato: req.body.data_contato,
+      observacao: req.body.observacao
+  }).then(function(){
+    res.redirect("/")
+  }).catch(function(erro){
+      res.send("Erro de cadastro dos dados: " + erro)
+  })
+})
+
+app.post("/atualizar", function(req, res){
+  post.update({
+      nome: req.body.nome,
+      telefone: req.body.telefone,
+      origem: req.body.origem,
+      data_contato: req.body.data_contato,
+      observacao: req.body.observacao
+  },{
+      where: {
+          id: req.body.id
+      }
+  }).then(function(){
+      res.redirect("/consulta")
+  })
+})
+
+app.get("/editar/:id", function(req, res){
+  post.findAll({where: {'id': req.params.id}}).then(function(post){
+      res.render("editar", {post})
+  }).catch(function(erro){
+      console.log("Erro de banco de dados: " + erro)
+  })
+})
+
+app.get("/consulta", function(req, res){
+  post.findAll().then(function(post){
+      res.render("consulta", {post})
+  }).catch(function(erro){
+      console.log("Erro de banco de dados: " + erro)
+  })
+})
+
+app.listen(8081, function(){
+  console.log("Servidor ativo!")
+})
